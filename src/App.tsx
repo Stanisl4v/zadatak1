3091d82dfda71dfdc1cbe7eabbf239b229af027b
Stanisl4v <slavko.stojsavljevic.sna@gmail.com>
2022-08-17 10:41:05 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+// const searchByCity = (event: { target: { value: string; }; }) => {
+//   festivals.filter(festival => {
+//     festival.city.toLowerCase().includes(event.target.value)
+//   })
+// }
+
+
+const festivals = [
+  {
+      name: "EXIT", 
+      month: "July", 
+      city: "Novi Sad", 
+      country: "Serbia"
+  },
+  {
+      name: "Sziget", 
+      month: "August", 
+      city: "Budapest", 
+      country: "Hungary"
+  },
+  {
+      name: "New Orelans Jazz & Heritage Festivals", 
+      month: "April", 
+      city: "New Orleans", 
+      country: "USA"
+  },
+  {
+      name: "Tomorrowland", 
+      month: "July", 
+      city: "Boom", 
+      country: "Belgium"
+  },
+  ]
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [searchTerm, setSearchTerm] = useState('')
+
+  return ( 
+  <div>
+    <input type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
+    {festivals.filter((val) => {
+      if(searchTerm == "") {
+        return val
+      } else if(val.city.toLocaleLowerCase().includes(searchTerm.toLowerCase())) {
+        return val
+      }
+    })
+      .map((val, key) => {
+      return ( 
+      <div key={key}>
+        <p>{val.city}</p>
+      </div>
+      );
+    })}
+  </div>
+  )
 }
+
 
 export default App;

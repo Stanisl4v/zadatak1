04b60587d85ea47bd22ff6ea038a55b4b582c0c0
Stanisl4v <slavko.stojsavljevic.sna@gmail.com>
2022-08-17 10:39:20 +0200

    Search bar, input search

---
+++ src/data.ts
@@ -0,0 +1,26 @@
+export const data = [
+{
+    name: "EXIT", 
+    month: "July", 
+    city: "Novi Sad", 
+    country: "Serbia"
+},
+{
+    name: "Sziget", 
+    month: "August", 
+    city: "Budapest", 
+    country: "Hungary"
+},
+{
+    name: "New Orelans Jazz & Heritage Festivals", 
+    month: "April", 
+    city: "New Orleans", 
+    country: "USA"
+},
+{
+    name: "Tomorrowland", 
+    month: "July", 
+    city: "Boom", 
+    country: "Belgium"
+},
+]
